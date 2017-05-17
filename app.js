6182ac1eeb0a00c69801408364b35bd7ea846a24
3m3kalionel <emeka.l.okoro@gmail.com>
2017-05-17 08:59:13 -0800

    create version 1 - necessary initial files

---
+++ app.js
@@ -0,0 +1,8 @@
+let newSession = document.querySelector("#add")
+var del = document.querySelector("#delete");
+// let firepadDiv = document.querySelector("#firepad")
+
+del.addEventListener('click', firepad.dispose());
+
+deleteSesion.addEventListener('click', console.log("afa"));
+
